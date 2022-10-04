8b4842ff0f8981c98ce8cf39e32060f386c6c637
Tomiris <98753027+tmsnn@users.noreply.github.com>
2022-10-04 23:15:59 +0600

    Update G.cpp

--- LAB1/G.cpp
+++ LAB1/G.cpp
@@ -1,28 +1,44 @@
-#include <bits/stdc++.h>  
-using namespace std;  
-int main(){  
-  int n;  
-  cin >> n;  
-  vector <int> primes;
-//   for(int i = 2; i <= n / 2; i++)  
-//     {  
-//       if(n % i != 0){ 
-//         primes.push_back(n);
-//         }  
-//     }  
-//   for(int i = 0; i < primes.size(); i++){
-//     cout << primes[i] << " ";
-//   }
-int i = 2;
-while(n >= 1){
-    if(n % i != 0){
-        primes.push_back(n);
-        i++;
-        n--;
+#include <bits/stdc++.h>
+using namespace std;
+bool isPrime(int n){
+    bool ok = false;;
+    for (int i = 2; i * i <= n; i++){
+        if (n % i == 0){
+            ok = true;
+            break;
+        }
+    }
+    if (n == 1) return false;
+    if (ok == false){
+        return true;
+    } else{
+        return false;
     }
 }
-reverse(primes.begin(), primes.end());
-for(int i = 0; i < primes.size(); i++){
-    cout << primes[i] << " ";
-}
+
+int main(){
+    // int cnt = 1;
+    int n; cin >> n;
+
+    vector <int> v;
+    vector <int> a;
+    int i = 1;
+
+    a.push_back(2);
+
+    while(i <= 100000) {
+        if (isPrime(i)) {
+            // cnt += 1;
+            a.push_back(i);
+        }        
+        i++;
+    }
+
+    for (int i = 1; i < a.size(); i++){
+        if (isPrime(i)) v.push_back(a[i]);
+    }
+
+    for (int i = 1; i < v.size(); i++){
+        if(i == n) cout << v[i-1];
+    }
 }
